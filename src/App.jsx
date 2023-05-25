53648e4913093c1b152748e2ae091f311cf1e925
Preeti Singh <109643586+psthegeek@users.noreply.github.com>
2023-05-25 18:21:04 +0530

    changes in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './app.scss';
-// import Register from './pages/register/Register';
-import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+// import Login from './pages/login/Login';
 // import Home from './pages/home/Home'
 // import Watch from './pages/watch/Watch'
 
 function App() {
   return (
           <>
-            <Login/>
+            <Register/>
           </>
   );
 }
